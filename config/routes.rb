028dd3b4e6e8ac15b7c97f1a093c91d949da4fcc
Siddharth Kaza <siddharthkaza@hotmail.com>
2015-04-04 22:34:55 -0400

    "make friend" is OK now! from Tao

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@
 
   devise_for :admins
   get 'admin/index'
-
-  devise_for :users
+  devise_for :users, :path=>'my'
   resources :posts
   resources :friendships
+  resources :users
 
     match ':controller/:action/:id', via: [:get, :post]
 
